8fc644b9f1a93bb8992a46c66ff7f5bf1c98a9fe
CentreMetre <luckyminer01@gmail.com>
2026-01-07 16:28:36 +0000

    Add project core/template files

---
+++ src/main/java/dev/centremetre/chapterio/ChapterioApplication.java
@@ -0,0 +1,15 @@
+package dev.centremetre.chapterio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterioApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ChapterioApplication.class, args);
+    }
+
+}
