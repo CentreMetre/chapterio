cf244e86548d4a67b55e6203e5e1dd02073a3ebc
CentreMetre <luckyminer01@gmail.com>
2026-01-07 16:28:28 +0000

    Add project core/template files

---
+++ src/test/java/dev/centremetre/chapterio/ChapterioApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.centremetre.chapterio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterioApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
